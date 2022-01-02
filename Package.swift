ef56806fcd33b0035a2a481dc8c7e7985d80f9a3
emin <emin.roblack@gmail.com>
2022-01-02 21:00:56 +0100

    test

--- Package.swift
+++ Package.swift
@@ -24,12 +24,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "btbTest",
-            path: "Sources/btbTest.xcframework"),
-        .target(
-            name: "Pand",
-            dependencies: []),
-        .testTarget(
-            name: "pandTests",
-            dependencies: ["Pand"]),
+            path: "Sources/btbTest.xcframework")
+//        .target(
+//            name: "Pand",
+//            dependencies: []),
+//        .testTarget(
+//            name: "pandTests",
+//            dependencies: ["Pand"]),
     ]
 )
