15724790989158b5db8f56f40e83b8bc40808790
emin <emin.roblack@gmail.com>
2022-01-02 22:11:05 +0100

    add watch target

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "Btb",
     platforms: [
         .macOS(.v12),
-        .iOS(.v13)
+        .iOS(.v13),
+        .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
