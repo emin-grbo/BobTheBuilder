9bd2053ee76ad9137c075d39a859d5477e6792a8
emin <emin.roblack@gmail.com>
2022-03-13 18:42:47 +0100

    add midnight blue

--- Sources/Bob/Colors.swift
+++ Sources/Bob/Colors.swift
@@ -1,15 +1,8 @@
-//
-//  File.swift
-//  
-//
-//  Created by Emin Grbo on 05/03/2022.
-//
-
 import Foundation
 import SwiftUI
 
 extension Color {
-    public static let boja = Color("Boja")
-    public static let boja2 = Color(hex: "ff44gg")
+
+    public static let midnightBlue_dark = Color(hex: "1B1C27")
     
 }
