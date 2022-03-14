b1ec12020eccb4faaa61e7ff25488191c1bac913
emin <emin.roblack@gmail.com>
2022-03-14 10:17:55 +0100

    Update Analytics.swift

--- Sources/Bob/Analytics.swift
+++ Sources/Bob/Analytics.swift
@@ -5,7 +5,7 @@ public enum App: String {
 }
 
 public struct Analytics {
-    public func logOpen(for app: App) {
+    public static func logOpen(for app: App) {
         guard let url = URL(string: "https://api.countapi.xyz/hit/\(app.rawValue)/open") else { return }
         let request = URLRequest(url: url)
         URLSession.shared.dataTask(with: request) { _, _, _ in }.resume()
