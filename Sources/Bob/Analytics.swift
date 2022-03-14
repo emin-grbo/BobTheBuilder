9b4da21102e5a5c8df171cc0230403d588397202
emin <emin.roblack@gmail.com>
2022-03-14 12:11:12 +0100

    add reset analytics

--- Sources/Bob/Analytics.swift
+++ Sources/Bob/Analytics.swift
@@ -1,15 +1,25 @@
 import Foundation
 
 public enum AppType: String {
-    case a11y
+    case accessibilityAssistant
 }
 
 public struct Analytics {
     public static func logOpen(for app: AppType) {
         Task {
-            guard let url = URL(string: "https://api.countapi.xyz/hit/\(app.rawValue)/open") else { return }
+            guard let url = URL(string: "https://api.countapi.xyz/hit/\(app.rawValue)/appOpen") else { return }
+            let session = URLSession.shared
+            guard let (_, _) = try? await session.data(from: url) else { return }
+        }
+    }
+    
+    public static func reset(for app: AppType) {
+        Task {
+            guard let url = URL(string: "https://api.countapi.xyz/set/\(app.rawValue)/appOpen?value=0") else { return }
             let session = URLSession.shared
             guard let (_, _) = try? await session.data(from: url) else { return }
         }
     }
 }
+
+//https://api.countapi.xyz/create?key=appOpen&namespace=a11y
