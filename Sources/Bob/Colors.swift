64ab8305d40dd024763f9e548899ab3841ca9454
emin <emin.grbo@tidal.com>
2022-03-15 22:18:46 +0100

    add new colors

--- Sources/Bob/Colors.swift
+++ Sources/Bob/Colors.swift
@@ -1,9 +1,13 @@
 import Foundation
 import SwiftUI
 
-extension Color {
+public extension Color {
 
-    public static let midnightBlue_dark = Color(hex: "1B1C27")
-    public static let nativeGray = Color(hex: "F7F7F7")
+    static let nativeGray = Color(hex: "F7F7F7")
+    
+    //MARK: Midnight
+    static let midnightBlue_dark = Color(hex: "1B1C27")
+    static let midnightBlue_mid = Color(hex: "2C2E47")
+    static let midnightBlue_light = Color(hex: "3E405E")
     
 }
