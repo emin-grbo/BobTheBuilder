3db7c93519065ca9beddb84e91c4ff01a413d038
emin <emin.roblack@gmail.com>
2022-03-15 09:52:46 +0100

    Update Date+Ex.swift

--- Sources/Bob/Date+Ex.swift
+++ Sources/Bob/Date+Ex.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension Date {
-    public func formatedToString() -> String {
+    func formatedToString() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "YY/MM/dd HH:mm"
         return dateFormatter.string(from: self)
