44b81ad757b56be047ce97ea04479dfa641f6a27
emin <emin.grbo@tidal.com>
2022-07-21 20:48:29 +0200

    remove analytics

--- Sources/Bob/Analytics.swift
+++ Sources/Bob/Analytics.swift
@@ -4,23 +4,3 @@ public enum AppType: String {
     case accessibilityAssistant
     case peep = "app.peep"
 }
-
-public struct Analytics {
-    public static func logOpen(for app: AppType) {
-        Task {
-            guard let url = URL(string: "https://api.countapi.xyz/hit/\(app.rawValue)/appOpen") else { return }
-            let session = URLSession.shared
-            guard let (_, _) = try? await session.data(from: url) else { return }
-        }
-    }
-    
-    public static func reset(for app: AppType) {
-        Task {
-            guard let url = URL(string: "https://api.countapi.xyz/set/\(app.rawValue)/appOpen?value=0") else { return }
-            let session = URLSession.shared
-            guard let (_, _) = try? await session.data(from: url) else { return }
-        }
-    }
-}
-
-//https://api.countapi.xyz/create?key=appOpen&namespace=app.peep&enable_reset=1
