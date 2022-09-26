67ace3eddc763b3b8515a5bd3fa5ce7d6c4a1a42
emin <emin@squareup.com>
2022-09-26 20:39:08 +0200

    make button styles public

--- Sources/Bob/MaterialSFButtonStyle.swift
+++ Sources/Bob/MaterialSFButtonStyle.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 
-struct MaterialSFButtonStyle: ButtonStyle {
+public struct MaterialSFButtonStyle: ButtonStyle {
     
   var sfString: String
   var bold: Bool = false
   var color: Color = Color.bob
   var size: CGFloat = 40
     
-    func makeBody(configuration: Self.Configuration) -> some View {
+  public func makeBody(configuration: Self.Configuration) -> some View {
       Image(systemName: sfString)
         .resizable()
         .font(.body.bold())
@@ -17,6 +17,6 @@ struct MaterialSFButtonStyle: ButtonStyle {
         .aspectRatio(contentMode: .fit)
         .opacity(configuration.isPressed ? 0.8 : 1)
         .scaleEffect(configuration.isPressed ? 1.2 : 1, anchor: .center)
-        .blur(radius: configuration.isPressed ? 1 : 0)
+        .blur(radius: configuration.isPressed ? 5 : 0)
     }
 }
