7f47af04d530cf3a6950f0657d89c673d56f28d7
emin <emin@squareup.com>
2022-10-06 12:13:28 +0200

    make a11y anim public

--- Sources/Bob/Global.swift
+++ Sources/Bob/Global.swift
@@ -1,9 +1,30 @@
 import SwiftUI
 
-func withReduceMotionAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
+public func withReduceMotionAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+  if UIAccessibility.isReduceMotionEnabled {
+    return try body()
+  } else {
+    return try withAnimation(animation, body)
+  }
 }
+
+//struct AnimationWithReduceMotion: ViewModifier {
+//
+//  var animation: Animation?
+//  let value: any Equatable
+//
+//  func body(content: Content) -> some View {
+//    content
+//      .animation2(animation, value: value)
+//  }
+//
+//
+//
+//}
+//
+//extension View {
+//  func animationWithReducedMotion<V>(_ animation: Animation?,
+//                                     value: V) -> some View where V : Equatable {
+//    modifier(AnimationWithReduceMotion(animation: animation, value: value))
+//  }
+//}
