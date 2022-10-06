a0a547ee633806e8d11e7dc54eba77528fc9dceb
emin <emin@squareup.com>
2022-10-06 09:58:42 +0200

    add withReduceMotionAnimation

---
+++ Sources/Bob/Global.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+func withReduceMotionAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
