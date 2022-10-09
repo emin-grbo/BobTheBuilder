266cb523580a871597421973d3260e450a6185a0
emin <emin@squareup.com>
2022-10-09 21:44:05 +0200

    fix material view syntax

--- Sources/Bob/MaterialText.swift
+++ Sources/Bob/MaterialText.swift
@@ -1,20 +1,25 @@
 import SwiftUI
 
 public struct MaterialText: ViewModifier {
+  
+  let font: Font
+  let color: Color
+  
   public func body(content: Content) -> some View {
     content
-      .font(.caption2.bold())
+      .font(font.bold())
       .padding(3)
       .padding(.horizontal, 1)
       .background(.quaternary)
       .cornerRadius(4)
-      .foregroundStyle(Color.orange)
+      .foregroundStyle(color)
       .minimumScaleFactor(0.5)
   }
 }
 
 extension View {
-  public func materialText() -> some View {
-    modifier(MaterialText())
+  public func materialText(font: Font = .caption2,
+                           color: Color = .bob) -> some View {
+    modifier(MaterialText(font: font, color: color))
   }
 }
