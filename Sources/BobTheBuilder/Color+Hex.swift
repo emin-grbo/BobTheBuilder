18ae8ff9f389c947b11aaac66e8cdfc64db9483f
emin <emin@grbo.space>
2023-08-08 13:45:58 +0200

    change color

--- Sources/BobTheBuilder/Color+Hex.swift
+++ Sources/BobTheBuilder/Color+Hex.swift
@@ -1,43 +1,29 @@
-import UIKit
 import SwiftUI
 
-extension UIColor {
-  public convenience init?(hex: String) {
-    var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-    hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-    
-    var rgb: UInt64 = 0
-    
-    var red: Double = 0.0
-    var green: Double = 0.0
-    var blue: Double = 0.0
-    var opacity: Double = 1.0
-    
-    let length = hexSanitized.count
-    
-    guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
-    
-    if length == 6 {
-      red = Double((rgb & 0xFF0000) >> 16) / 255.0
-      green = Double((rgb & 0x00FF00) >> 8) / 255.0
-      blue = Double(rgb & 0x0000FF) / 255.0
-      
-    } else if length == 8 {
-      red = Double((rgb & 0xFF000000) >> 24) / 255.0
-      green = Double((rgb & 0x00FF0000) >> 16) / 255.0
-      blue = Double((rgb & 0x0000FF00) >> 8) / 255.0
-      opacity = Double(rgb & 0x000000FF) / 255.0
-      
-    } else {
-      return nil
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
-    
-    self.init(red: red, green: green, blue: blue, alpha: opacity)
-  }
 }
 
-extension Color {
-  public init(hex: String) {
-    self.init(UIColor(hex: hex) ?? .white)
-  }
-}
