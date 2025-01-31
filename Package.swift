ea9b1ae1c3f80436887a36cc66daca8937275ed5
emin <emin@grbo.space>
2025-01-31 22:41:30 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BobTheBuilder",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13),
         .iOS(.v16),
         .watchOS(.v7)
     ],
